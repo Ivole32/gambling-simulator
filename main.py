8dd3b23f2d8516986789175566b3d2a81fa2a4e7
Ivole32 <167101401+Ivole32@users.noreply.github.com>
2025-06-24 23:02:01 +0200

    Added game over and house selling

--- main.py
+++ main.py
@@ -4,25 +4,39 @@ import random
 print("[green]Gambeling Simulator[/green]")
 
 money = 1000
+inventory = {"house": 275000}
 
 while True:
     print(f"[green]Money: {str(money)}$\n")
-    print("[red]Coin Flip:[/red]")
-    print("Bet amount: ", end="")
-    amount = int(input(""))
-    if amount <= money:
-        money -= amount
-        while True:
-            print("Coinflip: ")
-            random_flip = random.randint(1, 2)
-            if random_flip == 1:
-                amount *= 2
-                print("\n[green]You won![/green]")
-                if input("Flip again? (Y/N): ") == "Y":
-                    continue
-                else:
-                    money += amount
-                    break
+    if money <= 0:
+        if not inventory["house"] is None:
+            print("[red]Game Over! But wait you cann sell your house (Y/N): [/red]", end="")
+            if input("") == "Y":
+                money += inventory["house"]
+                inventory["house"] = None
+                continue
             else:
-                print("[red]You lost[/red]")
-                break
+                pass
+        print("[red bold]Game Over![/red bold]")
+        exit(0)
+    print("[green]Game Mode: [/green]\n1. Coin flip")
+    game = int(input("Mode: "))
+    if game == 1:
+        print("\n[red]Coin Flip:[/red]")
+        amount = int(input("Bet amount: "))
+        if amount <= money:
+            money -= amount
+            while True:
+                print("Coinflip: ")
+                random_flip = random.randint(1, 2)
+                if random_flip == 1:
+                    amount *= 2
+                    print("\n[green]You won![/green]")
+                    if input("Flip again? (Y/N): ") == "Y":
+                        continue
+                    else:
+                        money += amount
+                        break
+                else:
+                    print("[red]You lost[/red]")
+                    break
